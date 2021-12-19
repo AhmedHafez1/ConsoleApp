cdab7f86825db181f97cd35f951b136206f4acf5
ahafez <ahafez@tafeel.com>
2021-12-19 12:39:25 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new[] {1, 2, 3};
+            f.ta
+
+        }
+    }
+
+    enum Sport { Football, Baseball, Basketball, Hockey, Boxing, Rugby, Fencing }
+
+    class ManualSportSequence : IEnumerable<Sport>
+    {
+        public IEnumerator<Sport> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
